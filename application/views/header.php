3919dd23f33364912a5dee117ad3dd64c3b342a1
Ayu Permatasari <mayu.erizawa@gmail.com>
2022-05-13 08:31:18 +0700

    Add files via upload

---
+++ application/views/header.php
@@ -0,0 +1,162 @@
+
+<!DOCTYPE html>
+<html lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Title Page</title>
+
+        <!-- Bootstrap CSS -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.3/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <style>
+        .button2 {
+        background-color: Transparent;
+        border: none;
+        color: black;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
+        } 
+
+        .home {
+            position: absolute;
+            left: 55.9%;
+            right: 40.97%;
+            top: 0.94%;
+            bottom: 98.72%;
+
+            font-family: 'Inter';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 16px;
+            line-height: 19px;
+
+            /* #06C167 */
+
+            color: #06C167;
+        }
+
+        .about{
+            position: absolute;
+            left: 62.08%;
+            right: 32.99%;
+            top: 0.94%;
+            bottom: 98.72%;
+
+            font-family: 'Inter';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 16px;
+            line-height: 19px;
+
+            color: #1D1D1F;
+
+        }
+
+        .service{
+            position: absolute;
+            left: 69.65%;
+            right: 24.65%;
+            top: 0.94%;
+            bottom: 98.72%;
+
+            font-family: 'Inter';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 16px;
+            line-height: 19px;
+
+            color: #1D1D1F;
+        }
+
+        .company{
+            position: absolute;
+            left: 77.92%;
+            right: 14.86%;
+            top: 0.94%;
+            bottom: 98.72%;
+
+            font-family: 'Inter';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 16px;
+            line-height: 19px;
+
+            color: #1D1D1F;
+
+        }
+
+        .pages{
+            position: absolute;
+            left: 87.85%;
+            right: 8.89%;
+            top: 0.94%;
+            bottom: 98.72%;
+
+            font-family: 'Inter';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 16px;
+            line-height: 19px;
+
+            color: #1D1D1F;
+        }
+
+        .logo{
+            position: absolute;
+            left: 9.38%;
+            right: 82.29%;
+            top: 0.76%;
+            bottom: 98.54%;
+
+            font-family: 'Inter';
+            font-style: normal;
+            font-weight: 500;
+            font-size: 32px;
+            line-height: 39px;
+            /* identical to box height */
+
+
+            /* #06C167 */
+
+            color: #06C167;
+        }
+    </style>
+    
+    <body>
+        
+        
+        <div class="btn-toolbar" >
+            <div class="button2; logo" a href="#"> Foodies </div>
+            <div class="button2; home" a href="#"> Home </div>
+            <div class="button2; about" a href="#"> About Us</div>
+            <div class="button2; service" a href="#"> Service Us</div>
+            <div class="button2; company" a href="#"> Our Company</div>
+            <div class="button2; pages" a href="#"> Pages</div>
+            
+        </div>
+       
+
+        
+        
+
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Bootstrap JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </body>
+</html>
---
+++ application/views/home.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>The Foodies</title>
+
+        <!-- Bootstrap CSS -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.3/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    
+    <style>
+    
+    .home{
+        position: relative;
+        width: 1440px;
+        height: 5551px;
+    }
+    .title{
+        position: absolute;
+        left: 9.38%;
+        right: 53.61%;
+        top: 3.51%;
+        bottom: 93.21%;
+
+        font-family: 'Inter';
+        font-style: normal;
+        font-weight: 600;
+        font-size: 75px;
+        line-height: 91px;
+
+        width: 533px;
+        height: 182px;
+        top: 195px;
+        left: 135px;
+        
+
+        color: #1D1D1F;
+    }
+
+    .content{
+        position: absolute;
+        left: 9.38%;
+        right: 53.61%;
+        top: 6.99%;
+        bottom: 92.18%;
+
+        font-family: 'Inter';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 142.4%;
+
+        width: 533px;
+        height: 46px;
+        top: 388px;
+        left: 135px;
+        /* or 23px */
+
+
+        color: rgba(29, 29, 31, 0.75);
+    }
+
+    .button{
+        position: absolute;
+        left: 9.38%;
+        right: 76.74%;
+        top: 8.36%;
+        bottom: 90.42%;
+
+        width: 200px;
+        height: 68px;
+        top: 464px;
+        left: 135px;
+
+        /* #06C167 */
+
+        background: #06C167;
+        cursor: pointer;
+    }
+
+    .txtbutton{
+        position: absolute;
+        left: 11.51%;
+        right: 82.3%;
+        top: 8.79%;
+        bottom: 90.87%;
+
+        font-family: 'Inter';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 19px;
+
+        /* color: #FFFFFF; */
+
+        width: 89px;
+        height: 19px;
+        top: 488px;
+        left: 165.81px;
+    }
+
+    .feature{
+        position: absolute;
+        left: 9.38%;
+        right: 9.38%;
+        top: 16.42%;
+        bottom: 80.25%;
+
+        width: 1170px;
+        height: 185px;
+        top: 911.5px;
+        left: 135px;
+
+    }
+
+    .shipping{
+        position: absolute;
+        left: 9.38%;
+        right: 76.11%;
+        top: 17.46%;
+        bottom: 81.28%;
+
+        font-family: 'Inter';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 22px;
+        /* identical to box height */
+
+        color: #1D1D1F;
+
+        width:  117px;
+        height: 22px;
+        top: 982px;
+        left: 227px;
+    }
+
+    </style>
+
+    <body>
+        <div class="home"></div>
+        <div class="title"> We're Serious For Food</div>
+        <div class="content"> We strive to provide a space where guests can connect with themselves and explore their full potential, offering a safe place.</div>
+        <div class="button" > 
+            <div class="txtbutton"> Get started</div>
+        </div>
+
+        <div class="feature">
+            <div class="shipping">Free Shipping</div>
+            <div class="shipping">Free Shipping</div>
+            <div class="shipping">Free Shipping</div>
+            <div class="shipping">Free Shipping</div>
+            
+        </div>
+
+        </div>
+
+    
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Bootstrap JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </body>
+</html>
